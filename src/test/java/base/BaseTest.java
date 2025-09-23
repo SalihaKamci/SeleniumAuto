fa3f9cc3598cf34d7ce810a359450911b607c3d2
saliha <salihakamci06@gmail.com>
2025-09-23 18:53:32 +0300

    extentReport

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,18 +1,32 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
 
    protected  WebDriver  driver ;
+   protected static  ExtentReports extent;
+   protected ExtentTest test;
 
+   @BeforeSuite
+   public void SetupReport(){
+        extent = ExtentReportManager.getReportInstance();
+   }
+   @AfterSuite
+  public  void teardownReport(){
+       extent.flush();
+  }
  @BeforeMethod
-
   public void setUp (){
      Log.info("WebDriver starting");
      WebDriverManager.chromedriver().setup();
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,8 +3,10 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest {
@@ -12,12 +14,24 @@ public class LoginTest extends BaseTest {
     @Test
     public  void testValidLogin(){
         Log.info("LoginTest starting");
+        test = ExtentReportManager.createTest("Login Test");
+
+        test.info("navigation to URL");
         LoginPage loginpage = new LoginPage(driver);
+
+        Log.info("adding info");
+        test.info("adding info");
 
         loginpage.enterUserName("admin@yourstore.com");
         loginpage.enterUserPassword("admin");
+
+        test.info("clicking button");
         loginpage.clickLogin();
         System.out.println("page title " + driver.getTitle());
+        Log.info("verifying page title");
+        test.info("adding info");
+//        Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
+        test.pass("login successful");
 
     }
 }
